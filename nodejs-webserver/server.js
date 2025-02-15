f5a5f36eb467b0b2eb9351310d299a7bbd3d4498
Rahdriawan Prismana <63779468+prismana@users.noreply.github.com>
2025-02-15 22:40:13 +0700

    adding route, response header and response code

--- nodejs-webserver/server.js
+++ nodejs-webserver/server.js
@@ -1,28 +1,51 @@
 const http = require('http');
 
 const requestListener = (request, response) => {
-    response.setHeader('Content-Type', 'text/html');
+    response.setHeader('Content-Type', 'application/json');
+    response.setHeader('Powered-By', 'Node-js');
 
-    const { method } = request;
+    const {method, url} = request;
 
-    response.statusCode = 200
+    // response.statusCode = 200
 
-    if (method === "POST") {
-        let body = [];
+    // Route ke '\'
+    if (url === '/') {
+        if (method === 'GET') {
+            response.statusCode = 200
+            response.end('<h1>Ini adalah homepage</h1>')
+        } else {
+            response.statusCode = 400
+            response.end(`<h1>Halaman tidak dapat di akses dengan ${method} request </h1>`)
+        }
+    } // route about
+    else if (url === '/about') {
+        if (method === "POST") {
+            let body = [];
 
-        request.on('data', (chunk) => {
-            body.push(chunk);
-        });
+            request.on('data', (chunk) => {
+                body.push(chunk);
+            });
 
-        request.on('end', () => {
-            body = Buffer.concat(body).toString();
-            const { name } = JSON.parse(body);
-            response.end(`<h1>Hai, ${name}!</h1><br>\n`);
-        });
+            request.on('end', () => {
+                body = Buffer.concat(body).toString();
+                const {name} = JSON.parse(body);
+                response.statusCode = 200
+                response.end(`<h1>Hai, ${name}! ini adalah halaman about</h1><br>\n`);
+            });
+        } else if (method === "GET") {
+            response.statusCode = 200;
+            response.end('<h1>Halo! ini ini adalah halaman about</h1>')
+        } else {
+            response.statusCode = 400
+            response.end(`<h1>Halaman tidak dapat di akses dengan ${method} request </h1>`)
+        }
     }
-    if (method === "GET") {
-        response.end('<h1>Hai ini get</h1>')
+    // Respon url any, selain / dan /about
+    else {
+        response.statusCode = 404
+        response.end('<h1>Halaman tidak ditemukan</h1>')
     }
+
 }
 
 const server = http.createServer(requestListener);
